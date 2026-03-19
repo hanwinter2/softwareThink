f135edf6dc1c5bc6d00002596fb9851f23ec44a1
hanwinter <ohohminsheng@gmail.com>
2026-03-19 11:31:24 +0900

    a

---
+++ practice.py
@@ -0,0 +1,17 @@
+d = 0
+
+while True:
+    yut = list(map(int, input().split()))
+    yutNumber = 0
+
+    for i in range(len(yut)):
+        if yut[i] == 0:
+            yutNumber = yutNumber + 1
+
+    yutStr = ["모", "도", "개", "걸", "윷"]
+    print(yutStr[yutNumber])
+    d = d+1
+    
+    if d==3:
+        break
+    
