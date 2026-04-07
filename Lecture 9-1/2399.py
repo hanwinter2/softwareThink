308ed670baa93353374788d24f13c925c1487f15
hanwi <ohohminsheng@gmail.com>
2026-04-07 22:24:45 +0900

    Lecture 9-1

--- Lecture 9-1/2399.py
+++ Lecture 9-1/2399.py
@@ -1,11 +1,11 @@
 n = int(input())
 numbers = list(map(int, input().split()))
-numbers.sort
+numbers.sort()
 
-sum = 0
+total = 0
 
 for i in range(n-1):
     d = numbers[i+1] - numbers[i]
-    sum += d * (i+1) * (n-i-1)
+    total += d * (i+1) * (n-i-1)
 
-print(2 * sum)
+print(2 * total)
